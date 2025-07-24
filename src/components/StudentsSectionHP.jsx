c463b7fc9c9f35017cbfb69352a16910d7d5e7ec
angelavasilevska <163208425+angelavasilevska@users.noreply.github.com>
2025-07-24 11:47:46 +0200

    Edited Student Section- Home page

--- src/components/StudentsSectionHP.jsx
+++ src/components/StudentsSectionHP.jsx
@@ -7,18 +7,7 @@ const StudentsSectionHP = () => {
             {/* Горната секција - За студенти */}
             <div className="relative w-full max-w-full sm:max-w-[calc(100vw-8rem)] 2xl:h-[720px] h-[520px] mx-auto overflow-hidden rounded-xl">
                 {/* Икони (линиите) */}
-                {[53, 108, 163, 218].map((top, idx) => (
-                    <div
-                        key={idx}
-                        data-size="48"
-                        className={`w-[47px] h-10 absolute left-[48px] top-[${top}px] overflow-hidden`}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 24 24">
-                            <path fill="#5c5eaa" d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6z"/>
-                        </svg>
 
-                    </div>
-                ))}
 
                 {/* Наслов и текстови */}
                 <div>
@@ -33,23 +22,55 @@ const StudentsSectionHP = () => {
                         За студенти
                     </div>
                 </div>
-                <div className="left-[81px] top-[56px] absolute text-black text-xl font-normal font-['Inter']">
-                    Како да аплицираш (линк до е-услуги + кратко упатство со слики)
+                <div>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 24 24"
+                         className="mt-6 ml-10">
+                        <path fill="#5c5eaa" d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6z"/>
+                    </svg>
+                    <div className="left-[81px] top-[56px] absolute text-black text-xl font-normal font-['Inter']">
+                        Како да аплицираш (линк до е-услуги + кратко упатство со слики)
+                    </div>
                 </div>
-                <div className="left-[81px] top-[110px] absolute text-black text-xl font-normal font-['Inter']">
-                    ЧПП – Често поставувани прашања (Кога се објавуваат резултати, како да пријавиш дефект, итн.)
+                <div>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 24 24"
+                         className="mt-6 ml-10"
+                    >
+                        <path fill="#5c5eaa" d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6z"/>
+                    </svg>
+                    <div className="left-[81px] top-[110px] absolute text-black text-xl font-normal font-['Inter']">
+                        ЧПП – Често поставувани прашања (Кога се објавуваат резултати, како да пријавиш дефект, итн.)
+                    </div>
                 </div>
-                <div className="left-[81px] top-[166px] absolute text-black text-xl font-normal font-['Inter']">
-                    Внатрешен правилник / кодекс на однесување (PDF или HTML)
+                <div>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 24 24"
+                         className="mt-6 ml-10"
+                    >
+                        <path fill="#5c5eaa" d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6z"/>
+                    </svg>
+                    <div className="left-[81px] top-[166px] absolute text-black text-xl font-normal font-['Inter']">
+                        Внатрешен правилник / кодекс на однесување (PDF или HTML)
+                    </div>
                 </div>
-                <div className="left-[81px] top-[221px] absolute text-black text-xl font-normal font-['Inter']">
-                    Проблеми и пријави (линк до секцијата на управници и студентски управници во „За нас“)
+                <div>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 24 24"
+                   className="mt-6 ml-10"
+                    >
+                        <path fill="#5c5eaa" d="M8.59 16.58L13.17 12L8.59 7.41L10 6l6 6l-6 6z"/>
+                    </svg>
+                    <div className="left-[81px] top-[221px] absolute text-black text-xl font-normal font-['Inter']">
+                        Проблеми и пријави (линк до секцијата на управници и студентски управници во „За нас“)
+                    </div>
                 </div>
             </div>
 
             {/* Долната секција - Цитат и слика */}
             <div className="w-[1440px] h-[464px] absolute top-[340px] left-0">
-                <div className="relative w-full max-w-full sm:max-w-[calc(100vw-8rem)] 2xl:h-[720px] h-[520px] mx-auto overflow-hidden rounded-xl shadow-lg h-full absolute bg-[#a3a5d2] rounded-[15px] border-[3px] border-[#5c5eaa]" />
+                <div
+                    className="relative w-full max-w-full sm:max-w-[calc(100vw-8rem)] 2xl:h-[720px] h-[520px] mx-auto overflow-hidden rounded-xl shadow-lg h-full absolute bg-[#a3a5d2] rounded-[15px] border-[3px] border-[#5c5eaa]" />
 
                 <div className="w-[624px] h-[362px] left-[105px] top-[80px] absolute text-[#ebecf3] text-[31px] font-semibold font-['Inter']">
                     “Ние сме убедени дека домовите може да ги направиме поубаво место за живеење за сите студенти. Во таа насока, за
