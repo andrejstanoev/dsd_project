7993d3db07621a2c0b45a6dbfecd53098646fe5e
angelavasilevska <163208425+angelavasilevska@users.noreply.github.com>
2025-07-24 15:31:02 +0200

    Footer added

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavBar.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function AboutUs() {
     return(
         <div>
             <NavBar/>
             <h1>About us</h1>
+            <Footer/>
         </div>
     );
 }
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavBar.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function Contact() {
     return (
         <div>
             <NavBar/>
             <h1>Contact</h1>
+            <Footer/>
         </div>
     );
 }
