dbc6a3a940c5f7d2979c6230574201e5c8723ad8
Andrej Stanoev <stanoev.andrej12@gmail.com>
2025-07-27 18:19:10 +0200

    Changed icons in Contact page

--- src/components/EmailIcon.jsx
+++ src/components/EmailIcon.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+
 export default function EmailIcon() {
     return (
-        <svg className="w-6 h-6 text-[#5c5eaa]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <rect width="20" height="16" x="2" y="4" rx="2" ry="2" strokeWidth="2" stroke="currentColor"/>
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor"
-                  d="M22 6l-10 7L2 6"/>
+        // <svg className="w-6 h-6 text-[#5c5eaa]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        //     <rect width="20" height="16" x="2" y="4" rx="2" ry="2" strokeWidth="2" stroke="currentColor"/>
+        //     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor"
+        //           d="M22 6l-10 7L2 6"/>
+        // </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 512 512"
+             className="w-6 h-6 text-[#5c5eaa]"
+        >
+            <rect width="416" height="320" x="48" y="96" fill="none" stroke="#353767" stroke-linecap="round"
+                  stroke-linejoin="round" stroke-width="32" rx="40" ry="40"/>
+            <path fill="none" stroke="#353767" stroke-linecap="round" stroke-linejoin="round"
+                  stroke-width="32" d="m112 160l144 112l144-112"/>
         </svg>
-    )
+)
 }
--- src/components/LocationIcon.jsx
+++ src/components/LocationIcon.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+
 export default function LocationIcon() {
     return (
-        <svg className="w-6 h-6 text-[#5c5eaa]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                  d="M12 21c-4.97-5-8-8.13-8-11a8 8 0 1116 0c0 2.87-3.03 6-8 11z"/>
-            <circle cx="12" cy="9" r="2.5" fill="currentColor"/>
+        // <svg className="w-6 h-6 text-[#5c5eaa]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        //     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+        //           d="M12 21c-4.97-5-8-8.13-8-11a8 8 0 1116 0c0 2.87-3.03 6-8 11z"/>
+        //     <circle cx="12" cy="9" r="2.5" fill="currentColor"/>
+        // </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 16 16"
+             className="w-6 h-6 text-[#5c5eaa]"
+        >
+            <g fill="none" stroke="#353767" stroke-width="1">
+                <path d="M8 14.5C10.5 11 12.5 8 12.5 6a4.5 4.5 0 1 0-9 0c0 2 2 5 4.5 8.5Z"/>
+                <path d="M10 6a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z"/>
+            </g>
         </svg>
-    )
+)
 }
