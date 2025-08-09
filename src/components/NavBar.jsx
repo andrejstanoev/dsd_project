1076d7dc72cfff0b8c4c8594b4a40e9ffef36ccf
angelavasilevska <163208425+angelavasilevska@users.noreply.github.com>
2025-08-09 08:02:11 +0200

    Modify navbar layout and button positioning

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,7 +22,8 @@ export default function NavBar() {
             <Link to="/">
                 <img alt="logo" src={dsdlogo} className="h-23 mr-8"/>
             </Link>
-            <ul className=" hidden sm:flex 2xl:w-7xl lg:w-4xl xl:w-4xl  flex items-center  border-2 border-[#444692] bg-[#f4f5fa] rounded-full px-6 py-2 -mr-6 shadow-sm">
+
+            <ul className=" hidden sm:flex flex items-center justify-between border-2 border-[#444692] bg-[#f4f5fa] rounded-full px-6 py-2 shadow-sm gap-x-6">
                 <li className="mx-4">
                     <NavigationLink to="/">Почетна</NavigationLink>
                 </li>
@@ -58,15 +59,18 @@ export default function NavBar() {
                 <li className="mx-4">
                     <NavigationLink to="/meni">Дневно мени</NavigationLink>
                 </li>
-            </ul>
-            <button
-                className=" hidden sm:flex items-center justify-center w-14 h-14 rounded-full bg-[#ffd166] border-2 border-[#444692]">
+                <li>
+                    <button
+                        className=" hidden sm:flex items-center justify-center w-14 h-14 rounded-full bg-[#ffd166] border-2 border-[#444692]">
 
-                <svg className="w-7 h-7 text-[#444692]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <circle cx="11" cy="11" r="8" stroke="currentColor" strokeWidth="2"/>
-                    <line x1="25" y1="25" x2="16.65" y2="16.65" stroke="currentColor" strokeWidth="2"/>
-                </svg>
-            </button>
+                        <svg className="w-7 h-7 text-[#444692]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <circle cx="11" cy="11" r="8" stroke="currentColor" strokeWidth="2"/>
+                            <line x1="25" y1="25" x2="16.65" y2="16.65" stroke="currentColor" strokeWidth="2"/>
+                        </svg>
+                    </button>
+                </li>
+            </ul>
+
 
             {/*Hamburger ikona*/}
             <button onClick={handleToggleMenu}
