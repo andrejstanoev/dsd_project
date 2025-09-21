b12c50cecf6eea6c292fc39461c861d06517cd89
Andrej Stanoev <stanoev.andrej12@gmail.com>
2025-09-21 09:55:54 +0200

    Changed styling of the contact cards

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,7 +3,7 @@ import EmailIcon from "./EmailIcon.jsx";
 
 export default function ContactCard({ name, jobTitle, phone, email }) {
     return (
-        <div className="rounded-xl border-2 border-[#5c5eaa] bg-[#a3a5d2] p-4 w-full max-w-xl">
+        <div className="rounded-xl border border-[#dad8f0] ring-1 ring-indigo-300 shadow-lg xl:shadow-2xl bg-gradient-to-br from-[#a3a5d2] to-[#f2f2fc] p-4 w-full max-w-xl">
 
             <div className=" text-center rounded-xl bg-[#5C5EAA] py-2 px-4 mb-3">
                 <span className="text-white text-lg lg:text-[20px] xl:text-[23px] font-semibold">{name}</span>
@@ -21,11 +21,11 @@ export default function ContactCard({ name, jobTitle, phone, email }) {
             <div className="space-y-3">
                 <div className="flex items-center gap-2">
                     <PhoneIcon/>
-                    <span className="text-[#353767] font-medium xl:text-[20px]">{phone}</span>
+                    <span className="text-[#353767] font-semibold xl:text-[20px]">{phone}</span>
                 </div>
                 <div className="flex items-center gap-2">
                     <EmailIcon/>
-                    <span className="text-[#353767] font-medium break-all xl:text-[20px]">{email}</span>
+                    <span className="text-[#353767] font-semibold break-all xl:text-[20px]">{email}</span>
                 </div>
             </div>
         </div>
