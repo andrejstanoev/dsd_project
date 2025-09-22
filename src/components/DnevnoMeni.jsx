e745398136dbb6f42eaad80fa82339823cc75e62
Andrej Stanoev <stanoev.andrej12@gmail.com>
2025-09-22 20:53:33 +0200

    added translation to weeklumenu page

--- src/components/DnevnoMeni.jsx
+++ src/components/DnevnoMeni.jsx
@@ -13,7 +13,7 @@ const DnevnoMeni = () => {
                 </h1>
             </div>
 
-            <div className="flex gap-7 items-between justify-center">
+            <div className="flex flex-col lg:flex-row gap-7 items-between justify-center">
                 {/* слика */}
                 <div className=" max-w-xl my-8  flex justify-center rounded-xl border border-[#dad8f0] ring-1 ring-indigo-300 shadow-lg xl:shadow-2xl bg-gradient-to-br from-[#a3a5d2] to-[#f2f2fc] p-4 w-full">
                     <img className="border-3 border-[#5C5EAA] rounded-xl" src={dnevnoMeniImg} alt="Dnevno Meni" />
@@ -23,36 +23,30 @@ const DnevnoMeni = () => {
                     className="my-8 flex flex-col gap-5  ">
 
                     <section id="obrok-prijava" className="max-w-3xl mx-auto p-6 rounded-xl border border-[#dad8f0] ring-1 ring-indigo-300 shadow-lg xl:shadow-2xl bg-gradient-to-br from-[#a3a5d2] to-[#f2f2fc] p-4 w-full">
-                        <h3 className="text-2xl font-semibold mb-4">Електронска пријава за оброк</h3>
+                        <h3 className="text-2xl font-semibold mb-4">{t("elektronskaPrijavaZaObrok")}</h3>
 
                         <p className="mb-3">
-                            Секој студент е потребно да изврши <strong>електронска пријава за оброк</strong> преку
-                            формата со внесување на своето студентско {/*bla*/}
+                            {t("sekoiStudent")} <strong>{t("elektronskaPrijavaZaObrokSoMali")}</strong> {t("prekuFormata")} {/*bla*/}
                             <strong>ID</strong>.
                         </p>
 
                         <p className="mb-3">
-                            При пријавата, треба да одберете за кој ден ја пополнувате формата и дали ќе
-                            користите <strong>ручек, вечера или двата оброка</strong>.
+                            {t("zaKojDen")}<strong>{t("rucekVecera")}</strong>.
                         </p>
 
                         <p className="mb-3">
-                            Секој студент има на располагање <strong>три „џокери“</strong> во текот на месецот –
-                            можност да се пријави, а потоа да не дојде на оброк.
-                            По искористување на овие џокери, ќе следат соодветни последици доколку не се почитува
-                            системот.
+                            {t("sekojStudentImaNaRaspolaganje")} <strong>{t("triDzokeri")}</strong> {t("voTekotNaMesecot")}
                         </p>
 
                         <p className="mb-4">
-                            Оваа постапка се воведува со цел да се обезбеди попрецизна организација и да се намали
-                            непотребното фрлање на храна.
+                            {t("ovaaPostapka")}
                         </p>
 
                     </section>
                     <p>
                         <a href="URL_TO_FORM"
                            className="inline-block px-4 py-2 bg-[#5c5eaa] text-white rounded-md hover:opacity-90">
-                            Пополнете ја формата
+                            {t("popolnetaJaFormata")}
                         </a>
                     </p>
                 </div>
