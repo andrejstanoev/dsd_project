db8fd0fd7fa5cca58fcb47777c38a8b001920c08
Andrej Stanoev <stanoev.andrej12@gmail.com>
2025-09-22 20:01:18 +0200

    changes to the styling of types of rooms

--- src/components/NavigationLink.jsx
+++ src/components/NavigationLink.jsx
@@ -2,6 +2,6 @@ import {NavLink} from "react-router-dom";
 
 export default function NavigationLink({children, to} ){
     return(
-        <NavLink className="w-fit md:mx-2 lg:mx-3 font-bold text-[#353767] text-[10px] md:text-[14px] lg:text-[16px] xl:text-[22px] 2xl-[23px]  xl:px-4 xl:py-1 rounded-lg hover:bg-[#e8eafc]" to={to}>{children}</NavLink>
+        <NavLink className="w-fit md:mx-2 lg:mx-3 font-bold text-[#353767] text-[10xpx] md:text-[14px] lg:text-[16px] xl:text-[22px] 2xl-[23px]  xl:px-4 xl:py-1 rounded-lg hover:bg-[#e8eafc]" to={to}>{children}</NavLink>
     )
 }
